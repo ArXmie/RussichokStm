2febb783204710adfbee7947ea913f48c8536ea9
Студент группы IS <is@sielom.local>
2025-11-01 11:20:15 +0500

    html update 10

--- Russichok/steam_app/views.py
+++ Russichok/steam_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from .models
+from django.shortcuts import render, HttpResponse
+from .models import *
 
 # Create your views here.
 
 def community_login(request):
+    return render(request, "community_login.html")
     
